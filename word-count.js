60623dd04fab7a9c76612251d19375c8438431c9
George Yao <gyao@andrew.cmu.edu>
2022-12-01 02:04:57 -0500

    quick start on problem. Edge case with apostrophe to fix toms

--- word-count.js
+++ word-count.js
@@ -3,6 +3,18 @@
 // convenience to get you started writing code faster.
 //
 
-export const countWords = () => {
-  throw new Error('Remove this statement and implement this function');
+export const countWords = (words) => {
+  // 1. Underscore all + Tokenize string by splitting based on spec
+  // 1a. Specifically, use regex: [\w+'\w?]* means match any a-zA-Z0-9 character, with potentially
+  // an apostrophe followed immediately by a word char again; * for any number of times this group matches
+  // 2. Populate res obj with mapping <word: cnt>
+  let res = {}
+  words.toLowerCase().match(/[\w('\w{1)?]*/g).filter(Boolean).forEach((word) => {
+    if (res[word]) {
+      res[word]+=1
+    } else {
+      res[word] = 1
+    }
+  })
+  return res
 };
